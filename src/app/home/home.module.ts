6903810d84a544ffd8454bacab659cc15a4463ab
Mobile Developer <mobiledev@Mobile-Developer-1.local>
2018-11-30 17:01:37 -0500

    question 1

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-
 import { HomePage } from './home.page';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     RouterModule.forChild([
       {
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private stringForm: FormGroup;
+  private stringCheckResult: string;
+  private question1 = `
+  export model = new Model({
+    a:1,
+    b:function(){}
+  })
+  `;
+  private answer1: Array<string> = [];
+  private test1 = [null, undefined, '', 'Am I a string?'];
+  constructor(private formBuilder: FormBuilder) {
 
+  }
+
+  private runStringCheckSamples():void {
+    let i;
+    for (i = 0; i < this.test1.length; i++) { 
+        let test = this.checkNullOrEmpty(this.test1[i]);
+        let testName = this.test1[i];
+        let testResult = String(testName) + ':' + test;
+        this.answer1.push(testResult);
+    }
+    console.log(this.answer1)
+  }
+
+  private checkNullOrEmpty(value: string): string {
+    if (!!value) {
+      return 'You are a string!'
+    } else {
+      return 'You are not a string!'
+    }
+  }
+
+  ngOnInit() {
+    // this.stringForm = this.formBuilder.group({
+    //   'stringInput': ['', [Validators.required, Validators.pattern('[A-Za-z0-9]'), Validators.minLength(1)]],
+    // });
+  }
+
+  ngAfterViewInit() {
+    // this.stringForm
+    //   .valueChanges
+    //   .pipe(
+    //     distinctUntilChanged(),
+    //     debounceTime(500)
+    //   )
+    //   .subscribe(formValues => {
+    //    this.stringCheckResult = this.checkNullOrEmpty(formValues.stringInput);
+    //   });
+  }
 }
