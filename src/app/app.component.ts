d5aad873500116518cb8be00678a8dcd87d610f7
Mobile Developer <mobiledev@Mobile-Developer-1.local>
2018-12-04 15:39:19 -0500

    navigate update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
+      title: 'Q&A',
       url: '/home',
-      icon: 'home'
+      icon: 'list'
     },
     {
       title: 'About',
